790032d30ac320cff044934e8a7d6ae50874a3d3
makharashvilin <163711814+makharashvilin@users.noreply.github.com>
2024-11-20 16:08:18 +0400

    Add files via upload

---
+++ hr/employees/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    salary = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.job_title}"
+
+
